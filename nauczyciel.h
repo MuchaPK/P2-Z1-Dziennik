8ced7ac18cc7bcea59f352c34107e9af2dc48758
Dawid Jarzyna <dawid.jarzyna@student.pk.edu.pl>
2026-01-05 19:53:26 +0100

    Usuwanie oraz edycja ocen

--- nauczyciel.h
+++ nauczyciel.h
@@ -11,6 +11,104 @@ class Nauczyciel : public Uzytkownik {
 public:
     Nauczyciel(string l, string h, string i, string n) : Uzytkownik(l, h, i, n) {}
 
+    // Funkcja zapisująca stan faktyczny (z pamięci RAM) do pliku tekstowego
+    void zapiszWszystkieOcenyDoPliku(vector<Uzytkownik*>& baza) {
+        ofstream plik("oceny.txt", ios::trunc); // Czyścimy plik
+        if (!plik.is_open()) return;
+
+        for (Uzytkownik* u : baza) {
+            // Rzutujemy na Ucznia, żeby dostać się do dzienniczka
+            Uczen* ucz = dynamic_cast<Uczen*>(u);
+            if (ucz) {
+                // Dzięki "friend class" mamy bezpośredni dostęp do ucz->dzienniczek
+                for (const auto& o : ucz->dzienniczek) {
+                    plik << ucz->getLogin() << ";"
+                         << o.przedmiot << ";"
+                         << o.symbol << ";"
+                         << o.opis << ";"
+                         << o.data << ";"
+                         << o.waga << endl;
+                }
+            }
+        }
+        plik.close();
+        cout << " [i] Zapisano zmiany w pliku oceny.txt\n";
+    }
+
+    // Pomocnicza funkcja do wyboru ucznia z listy
+    Uczen* wybierzUcznia(vector<Uzytkownik*>& baza) {
+        vector<Uczen*> listaUczniow;
+        int i = 1;
+        cout << "\n--- LISTA UCZNIOW ---\n";
+        for (Uzytkownik* u : baza) {
+            Uczen* ucz = dynamic_cast<Uczen*>(u);
+            if (ucz) {
+                cout << i++ << ". " << ucz->getImie() << " " << ucz->getNazwisko()
+                     << " (" << ucz->getLogin() << ")\n";
+                listaUczniow.push_back(ucz);
+            }
+        }
+        if (listaUczniow.empty()) { cout << "Brak uczniow.\n"; return nullptr; }
+
+        cout << "Wybierz numer ucznia (0 anuluje): ";
+        int wybor = Sys::pobierzInt();
+        if (wybor < 1 || wybor > listaUczniow.size()) return nullptr;
+
+        return listaUczniow[wybor - 1];
+    }
+
+    void edytujOcene(vector<Uzytkownik*>& baza) {
+        Uczen* ucz = wybierzUcznia(baza);
+        if (!ucz) return;
+
+        if (ucz->dzienniczek.empty()) { cout << "Ten uczen nie ma ocen.\n"; return; }
+
+        // Wyświetlanie ocen (dostęp bezpośredni dzięki friend)
+        cout << "\nOceny ucznia " << ucz->getImie() << ":\n";
+        for (int i = 0; i < ucz->dzienniczek.size(); i++) {
+            cout << i + 1 << ". ";
+            ucz->dzienniczek[i].wypisz();
+        }
+
+        cout << "Wybierz numer oceny do edycji: ";
+        int nr = Sys::pobierzInt();
+        if (nr < 1 || nr > ucz->dzienniczek.size()) { cout << "Zly numer.\n"; return; }
+
+        // Edycja bezpośrednia na wektorze
+        auto& ocena = ucz->dzienniczek[nr - 1];
+        cout << "Edytujesz: " << ocena.przedmiot << " (" << ocena.symbol << ")\n";
+
+        cout << "Nowa ocena (symbol, np. 4+): "; cin >> ocena.symbol;
+        cout << "Nowy opis: "; cin.ignore(); getline(cin, ocena.opis);
+        cout << "Nowa waga: "; ocena.waga = Sys::pobierzInt();
+
+        // Przeliczenie wartości liczbowej (korzystamy z metody w strukturze Ocena)
+        ocena.wartosc = ocena.przeliczNaLiczbe(ocena.symbol);
+
+        zapiszWszystkieOcenyDoPliku(baza); // Zapis do pliku
+    }
+
+    void usunOcene(vector<Uzytkownik*>& baza) {
+        Uczen* ucz = wybierzUcznia(baza);
+        if (!ucz) return;
+        if (ucz->dzienniczek.empty()) { cout << "Brak ocen.\n"; return; }
+
+        cout << "\nOceny do usuniecia:\n";
+        for (int i = 0; i < ucz->dzienniczek.size(); i++) {
+            cout << i + 1 << ". "; ucz->dzienniczek[i].wypisz();
+        }
+
+        cout << "Podaj numer oceny do USUNIECIA: ";
+        int nr = Sys::pobierzInt();
+        if (nr < 1 || nr > ucz->dzienniczek.size()) { cout << "Zly numer.\n"; return; }
+
+        // Usuwanie z wektora
+        ucz->dzienniczek.erase(ucz->dzienniczek.begin() + (nr - 1));
+        cout << "Usunieto ocene.\n";
+
+        zapiszWszystkieOcenyDoPliku(baza); // Zapis do pliku
+    }
+
     void pokazMenu(vector<Uzytkownik*>& baza) override {
         int wybor = 0;
         while (wybor != 3) {
@@ -20,13 +118,20 @@ public:
             cout << "========================================\n";
             cout << "1. Wystaw ocene\n";
             cout << "2. Lista uczniow\n";
-            cout << "3. Wyloguj\n";
+            cout << "3. Edytuj ocene\n";
+            cout << "4. Usun ocene\n";
+            cout << "5. Wyloguj\n";
             cout << "wybor > ";
             wybor = Sys::pobierzInt();
 
-            if (wybor == 1) wystawOcene(baza);
+            if (wybor == 1) {
+                wystawOcene(baza);
+                zapiszWszystkieOcenyDoPliku(baza);
+            }
             else if (wybor == 2) wypiszUczniow(baza);
-            else if (wybor == 3) break;
+            else if (wybor == 3) edytujOcene(baza);
+            else if (wybor == 4) usunOcene(baza);
+            else if (wybor == 5) break;
             else {
                 cout << "Nie ma takiej opcji!\n";
                 Sys::pauza();
@@ -38,7 +143,7 @@ public:
         cout << "\nLista uczniow w systemie:\n";
         for (Uzytkownik* u : baza) {
             if (dynamic_cast<Uczen*>(u)) {
-                cout << "- " << u->getImie() << " " << u->getNazwisko() 
+                cout << "- " << u->getImie() << " " << u->getNazwisko()
                      << " (Login: " << u->getLogin() << ") \n";
             }
         }
@@ -67,14 +172,14 @@ public:
 
         string przedmiot, symbol, opis, data;
         int waga;
-        
+
         cout << "Przedmiot: "; cin >> przedmiot;
         cout << "Ocena (np. 4+): "; cin >> symbol;
         cout << "Waga: "; cin >> waga;
         cout << "Data: "; cin >> data;
-        cout << "Opis (jedno slowo lub uzyj podkreslen): "; cin >> opis; 
+        cout << "Opis (jedno slowo lub uzyj podkreslen): "; cin >> opis;
 
-        
+
         wybranyUczen->dodajOcene(przedmiot, symbol, opis, waga, data);
         ofstream ocenki("../oceny.txt", std::ios_base::app | std::ios_base::out);
         ocenki << "\n" << loginUcznia << ";" << przedmiot << ";" << symbol << ";" <<  opis << ";" << data << ";" << waga << ";";
